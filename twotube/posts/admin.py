5d3b06f5f1e8ee36214289883c56d21e677bdfa7
Zero <zed.work@mail.ru>
2021-09-02 16:03:04 +0300

    Add pk to list_display in PostAdmin

--- twotube/posts/admin.py
+++ twotube/posts/admin.py
@@ -4,11 +4,13 @@ from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
     # перечисляем поля, которые будут отображаться в админке
-    list_display = ("text", "pub_date", "author")
+    list_display = ("pk","text", "pub_date", "author")
     # добавляем интерфейс для поиска по тексту постов
     search_fields = ("text",)
     # Добавляем возможность фильтрации по дате
     list_filter = ("pub_date",)
+    empty_value_display = "-пусто-" # это свойство сработает для всех колонок:
+                                    #где пусто - там будет эта строка
 
 # при регистрации модели Post источником конфигурации для неё 
 # назначаем класс PostAdmin
