2292eca365fe6c2fea635fdfd3e6249686fbf60d
Zero <zed.work@mail.ru>
2021-09-02 15:29:05 +0300

    Create superuser,  Register and Configurate Post in admin

--- twotube/posts/admin.py
+++ twotube/posts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+# из файла models импортируем модель Post
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые будут отображаться в админке
+    list_display = ("text", "pub_date", "author")
+    # добавляем интерфейс для поиска по тексту постов
+    search_fields = ("text",)
+    # Добавляем возможность фильтрации по дате
+    list_filter = ("pub_date",)
+
+# при регистрации модели Post источником конфигурации для неё 
+# назначаем класс PostAdmin
+admin.site.register(Post, PostAdmin)
