1bdefb0aa6de0825d091872e63878f69df9b30ae
Zero <zed.work@mail.ru>
2021-09-06 19:17:46 +0300

    Changed views and add there render

--- twotube/posts/views.py
+++ twotube/posts/views.py
@@ -1,13 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Post
 
 
 def index(request):
-
+    a = 1
     latest = Post.objects.order_by('-pub_date')[:10]
-    # собираем тексты постов в один, разделяя новой строкой
-    output = []
-    for item in latest:
-        output.append(item.text)
-    return HttpResponse('\n'.join(output))
+    # That's correct way to use render func
+    return render(request, "index.html", {"posts": latest}) 
+    
