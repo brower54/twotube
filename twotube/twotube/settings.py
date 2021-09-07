7a3206a39efde2bb738d53f181477fca8cc90b8b
Zero <zed.work@mail.ru>
2021-09-07 11:30:34 +0300

    Added static files for posts app

--- twotube/twotube/settings.py
+++ twotube/twotube/settings.py
@@ -56,7 +56,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR,],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
