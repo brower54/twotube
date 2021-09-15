7b34c0e4980049fcc5ae48e88249e1a529d52062
Zero <zed.work@mail.ru>
2021-09-15 11:21:52 +0300

    Delete ussless comments

--- twotube/posts/views.py
+++ twotube/posts/views.py
@@ -6,6 +6,4 @@ from .models import Post
 def index(request):
     a = 1
     latest = Post.objects.order_by('-pub_date')[:10]
-    # That's correct way to use render func
     return render(request, "index.html", {"posts": latest}) 
-    
