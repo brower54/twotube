10d527269c3838f49e7125519ec4b74b8bb83ac8
Zero <zed.work@mail.ru>
2021-09-16 09:33:59 +0300

    Added path for group_posts

--- twotube/posts/urls.py
+++ twotube/posts/urls.py
@@ -1,7 +1,9 @@
+from os import name
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("group/<slug:slug>/", views.views.group_posts, name="group_posts"),
 ]
