7824fccedebeaef8d6da52c1327f67032e4ab70d
Zero <zed.work@mail.ru>
2021-09-22 09:38:25 +0300

    Created app users and included it in settings

--- twotube/twotube/settings.py
+++ twotube/twotube/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
+    'posts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts',  # Our app posts**
 ]
 
 MIDDLEWARE = [
